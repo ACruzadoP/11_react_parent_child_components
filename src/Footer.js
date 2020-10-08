c2bb624d071f2a04a54a7b745f170422426b356d
Alberto <albertocruzadopleite@gmail.com>
2020-10-08 04:53:59 +0200

    Tomando snapshot de un ejercicio terminado

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function Footer() {
+    return (
+        <footer>
+            <h3>This is going to be the Footer</h3>
+        </footer>
+    )
+}
+
+export default Footer
